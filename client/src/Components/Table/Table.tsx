0f19143f3a162db80d82349fbf5fd510291db92d
Uiguuna Mikhailova <uyguuna1997@yandex.ru>
2022-10-21 01:50:27 +0900

    feat: add table and table items and add some styles for them

--- client/src/Components/Table/Table.tsx
+++ client/src/Components/Table/Table.tsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import Pagination from 'Components/Pagination/Pagination';
+import TableItem from 'Components/TableItem/TableItem';
+import React, { useState } from 'react';
+import './Table.css';
 
-export default function Table() {
+type Item = {
+  date: string;
+  name: string;
+  quantity: number;
+  distance: number;
+  id: number;
+};
+
+type TableProps = {
+  items: Item[];
+};
+
+export default function Table(props: TableProps) {
   return (
-    <div>Table</div>
-  )
+    <>
+      <table className="table">
+        <tr className="table__item">
+          <th className="table__item-title">Дата</th>
+          <th className="table__item-title">Название</th>
+          <th className="table__item-title">Количество</th>
+          <th className="table__item-title">Дистанция</th>
+        </tr>
+        {props.items.map((item) => {
+          return (
+            <TableItem
+              key={item.id}
+              date={item.date}
+              name={item.name}
+              quantity={item.quantity}
+              distance={item.distance}
+            />
+          );
+        })}
+      </table>
+      <Pagination />
+    </>
+  );
 }
