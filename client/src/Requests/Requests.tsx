52b578d45da486486d0adaebf45f400d5fc9c280
Uiguuna Mikhailova <uyguuna1997@yandex.ru>
2022-10-22 00:16:44 +0900

    feat: add local data if fetch failed

--- client/src/Requests/Requests.tsx
+++ client/src/Requests/Requests.tsx
@@ -1,5 +1,13 @@
+import LocalItems from './LocalItems';
+
 export default async function getData() {
-  const res = await fetch('http://localhost:5000/api/items');
-  const data = await res.json();
-  return data;
+  try {
+    const res = await fetch('http://localhost:5000/api/items');
+    const data = await res.json();
+    console.log(data);
+    return data;
+  } catch (e) {
+    console.log(e);
+    return LocalItems;
+  }
 }
