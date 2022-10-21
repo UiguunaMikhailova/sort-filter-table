613a645665993396d8ac82b011ecd66332789be8
Uiguuna Mikhailova <uyguuna1997@yandex.ru>
2022-10-21 22:47:36 +0900

    feat: add text when after filters array is empty

--- client/src/Components/Home/Home.tsx
+++ client/src/Components/Home/Home.tsx
@@ -1,170 +1,163 @@
 import React, { useEffect, useState } from 'react';
 import Table from 'Components/Table/Table';
 import getData from 'Requests/Requests';
-import './Home.css';
 import { Item } from 'types/types';
 import Form from 'Components/Form/Form';
 import { sortItems } from 'Helpers/Helpers';
+import './Home.css';
 
 export default function Home() {
-  const [items, setItems] = useState([]);
-  const [initialItems, setInitialItems] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [itemsPerPage] = useState(5);
+  const [items, setItems] = useState([]); // массив данных удовлетворяющий всем критериям
+  const [initialItems, setInitialItems] = useState([]); // изначальный массив данных
+  const [currentPage, setCurrentPage] = useState(1); // текущая страница
+  const [itemsPerPage] = useState(5); // максимальное количество элементов на одной странице таблицы
+  const [isEmpty, setIsEmpty] = useState(false); // если массив оказался пустым после установки критериев сортировки и фильтрации
 
   useEffect(() => {
-    getItems();
-  });
+    getData().then((data) => {
+      if (data) {
+        setItems(data);
+        setInitialItems(data);
+      }
+    });
+  }, []);
 
-  function getItems() {
-    if (items.length) {
-      setItems(items);
-    } else {
-      getData().then((data) => {
-        if (data) {
-          setItems(data);
-          setInitialItems(data);
-        }
-      });
-    }
-  }
+  const lastItemIndex = currentPage * itemsPerPage; // до куда слайсить массив
+  const firstItemIndex = lastItemIndex - itemsPerPage; // откуда слайсить массив
+  const currentItems = items.slice(firstItemIndex, lastItemIndex); // 5 элементов, которые должны быть на текущей странице
+  const lastPage = Math.ceil(items.length / itemsPerPage); // номер последней страницы
 
-  const lastItemIndex = currentPage * itemsPerPage;
-  const firstItemIndex = lastItemIndex - itemsPerPage;
-  const currentItems = items.slice(firstItemIndex, lastItemIndex);
-  const lastPage = Math.ceil(items.length / itemsPerPage);
-
-  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
-  const nextPage = () => setCurrentPage((prev) => (prev < lastPage ? prev + 1 : 1));
-  const prevPage = () => setCurrentPage((prev) => (prev > 1 ? prev - 1 : lastPage));
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber); // установка текущей страницы
+  const nextPage = () => setCurrentPage((prev) => (prev < lastPage ? prev + 1 : 1)); // увеличение текущей страницы
+  const prevPage = () => setCurrentPage((prev) => (prev > 1 ? prev - 1 : lastPage)); // уменьшение текущей страницы
 
   function updateItems(sort: string, firstValue: string, secondValue: string, input: string) {
+    setIsEmpty(false);
     setItems([...initialItems]);
-    const filteredItems: never[] = [];
-    filteredItems.push(...sortItems(sort, initialItems));
-    filterItems(filteredItems, firstValue, secondValue, input);
+    const filteredItems = [...sortItems(sort, initialItems)]; // отсортированный массив
+    filterItems(filteredItems, firstValue, secondValue, input); // фильтруем массив
   }
 
   function filterItems(itemsArr: never[], firstValue: string, secondValue: string, input: string) {
+    let result: never[] = [];
     if (secondValue === 'contains') {
-      filterItemsContains(firstValue, itemsArr, input);
+      result = filterItemsContains(firstValue, itemsArr, input); // элемент массива содержит значение
     }
 
     if (secondValue === 'equals') {
-      filterItemsEquals(firstValue, itemsArr, input);
+      result = filterItemsEquals(firstValue, itemsArr, input); // элемент массива равен значению
     }
 
     if (secondValue === 'more') {
-      filterItemsMore(firstValue, itemsArr, input);
+      result = filterItemsMore(firstValue, itemsArr, input); // элемент массива больше значения
     }
     if (secondValue === 'less') {
-      filterItemsLess(firstValue, itemsArr, input);
+      result = filterItemsLess(firstValue, itemsArr, input); // элемент массива меньше значения
+    }
+
+    if (!result.length) {
+      setIsEmpty(true); // если после фильтрации массив оказался пустым (не нашлось элементов удовлетворяющих критериям)
+    } else {
+      setItems([...result]);
     }
   }
 
-  function filterItemsContains(value: string, itemsArr: never[], input: string) {
+  // элемент массива содержит значение
+  function filterItemsContains(value: string, itemsArr: never[], input: string): never[] {
     if (value === 'name') {
-      setItems([
-        ...itemsArr.filter((item: Item) => item.name.toLowerCase().includes(input.toLowerCase())),
-      ]);
+      return itemsArr.filter((item: Item) => item.name.toLowerCase().includes(input.toLowerCase()));
     }
     if (value === 'date') {
-      setItems([
-        ...itemsArr.filter((item: Item) => item.date.toLowerCase().includes(input.toLowerCase())),
-      ]);
+      return itemsArr.filter((item: Item) => item.date.toLowerCase().includes(input.toLowerCase()));
     }
     if (value === 'quantity') {
-      setItems([
-        ...itemsArr.filter((item: Item) =>
-          item.quantity.toString().toLowerCase().includes(input.toLowerCase())
-        ),
-      ]);
+      return itemsArr.filter((item: Item) =>
+        item.quantity.toString().toLowerCase().includes(input.toLowerCase())
+      );
     }
     if (value === 'distance') {
-      setItems([
-        ...itemsArr.filter((item: Item) =>
-          item.distance.toString().toLowerCase().includes(input.toLowerCase())
-        ),
-      ]);
+      return itemsArr.filter((item: Item) =>
+        item.distance.toString().toLowerCase().includes(input.toLowerCase())
+      );
     }
+    return [];
   }
 
-  function filterItemsEquals(value: string, itemsArr: never[], input: string) {
+  // элемент массива равен значению
+  function filterItemsEquals(value: string, itemsArr: never[], input: string): never[] {
     if (value === 'name') {
-      setItems([
-        ...itemsArr.filter((item: Item) => item.name.toLowerCase() === input.toLowerCase()),
-      ]);
+      return itemsArr.filter((item: Item) => item.name.toLowerCase() === input.toLowerCase());
     }
     if (value === 'date') {
-      setItems([
-        ...itemsArr.filter((item: Item) => item.date.toLowerCase() === input.toLowerCase()),
-      ]);
+      return itemsArr.filter((item: Item) => item.date.toLowerCase() === input.toLowerCase());
     }
     if (value === 'quantity') {
-      setItems([
-        ...itemsArr.filter(
-          (item: Item) => item.quantity.toString().toLowerCase() === input.toLowerCase()
-        ),
-      ]);
+      return itemsArr.filter(
+        (item: Item) => item.quantity.toString().toLowerCase() === input.toLowerCase()
+      );
     }
     if (value === 'distance') {
-      setItems([
-        ...itemsArr.filter(
-          (item: Item) => item.distance.toString().toLowerCase() === input.toLowerCase()
-        ),
-      ]);
+      return itemsArr.filter(
+        (item: Item) => item.distance.toString().toLowerCase() === input.toLowerCase()
+      );
     }
+    return [];
   }
 
-  function filterItemsMore(value: string, itemsArr: never[], input: string) {
+  // элемент массива больше значения
+  function filterItemsMore(value: string, itemsArr: never[], input: string): never[] {
     if (value === 'name') {
-      setItems([...itemsArr.filter((item: Item) => item.name.toLowerCase() > input.toLowerCase())]);
+      return itemsArr.filter((item: Item) => item.name.toLowerCase() > input.toLowerCase());
     }
     if (value === 'date') {
-      setItems([
-        ...itemsArr.filter((item: Item) => {
-          return Date.parse(item.date) >= Date.parse(input);
-        }),
-      ]);
+      return itemsArr.filter((item: Item) => {
+        return Date.parse(item.date) >= Date.parse(input);
+      });
     }
     if (value === 'quantity') {
-      setItems([...itemsArr.filter((item: Item) => item.quantity >= Number(input))]);
+      return itemsArr.filter((item: Item) => item.quantity >= Number(input));
     }
     if (value === 'distance') {
-      setItems([...itemsArr.filter((item: Item) => item.distance >= Number(input))]);
+      return itemsArr.filter((item: Item) => item.distance >= Number(input));
     }
+    return [];
   }
 
-  function filterItemsLess(value: string, itemsArr: never[], input: string) {
+  // элемент массива меньше значения
+  function filterItemsLess(value: string, itemsArr: never[], input: string): never[] {
     if (value === 'name') {
-      setItems([...itemsArr.filter((item: Item) => item.name.toLowerCase() < input.toLowerCase())]);
+      return itemsArr.filter((item: Item) => item.name.toLowerCase() < input.toLowerCase());
     }
     if (value === 'date') {
-      setItems([
-        ...itemsArr.filter((item: Item) => {
-          return Date.parse(item.date) <= Date.parse(input);
-        }),
-      ]);
+      return itemsArr.filter((item: Item) => Date.parse(item.date) <= Date.parse(input));
     }
     if (value === 'quantity') {
-      setItems([...itemsArr.filter((item: Item) => item.quantity <= Number(input))]);
+      return itemsArr.filter((item: Item) => item.quantity <= Number(input));
     }
     if (value === 'distance') {
-      setItems([...itemsArr.filter((item: Item) => item.distance <= Number(input))]);
+      return itemsArr.filter((item: Item) => item.distance <= Number(input));
     }
+    return [];
   }
+
   return (
     <div className="home">
       <Form updateItems={updateItems} />
-      <Table
-        items={currentItems}
-        paginate={paginate}
-        nextPage={nextPage}
-        prevPage={prevPage}
-        itemsPerPage={itemsPerPage}
-        totalItems={items.length}
-        currentPage={currentPage}
-      />
+      {isEmpty ? (
+        <div className="empty">
+          По вашему запросу ничего не найдено, пожалуйста, поменяйте настройки
+        </div>
+      ) : (
+        <Table
+          items={currentItems}
+          paginate={paginate}
+          nextPage={nextPage}
+          prevPage={prevPage}
+          itemsPerPage={itemsPerPage}
+          totalItems={items.length}
+          currentPage={currentPage}
+        />
+      )}
     </div>
   );
 }
